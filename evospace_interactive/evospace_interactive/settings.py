715182b24e107476ec153b885bef4b3fa7fd6806
jcromerohdz <jcromerohdz@gmail.com>
2014-11-18 16:13:47 -0800

    Update settings.py

--- evospace_interactive/evospace_interactive/settings.py
+++ evospace_interactive/evospace_interactive/settings.py
@@ -99,8 +99,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'evospace_i_data',
-        'USER': 'chris',
+        'NAME': 'evospaceInteractive_DB',
+        'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
